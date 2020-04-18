df4f107b04c006a16d0146f8b7c8208522dbc987
Vianpyro <10519369+Vianpyro@users.noreply.github.com>
2020-04-18 13:25:25 -0400

    Translate love.bat

--- love.bat
+++ love.bat
@@ -1,17 +1,17 @@
 @echo off
 color 57
-echo Hey, est-ce que tu m'aimes ? (repond seulement avec "oui" ou "non")
+echo Hey, do you love me ? (answer only with "yes" or "not")
 set /p love=
 if %love%==oui goto love
 if %love%==non goto hate
 :love
-echo Je t'aime aussi...
-echo A bientot :)
+echo I love you too...
+echo See you soon :)
 pause
 exit
 :hate
-echo Mais je t'aime... Hehehehe
-echo Tu as ete pirate(e)...
-echo Ton PC vas s'eteindre dans 10 secondes...
+echo But... I love you... Hehehehe
+echo You got hacked...
+echo Your computer will shutdown in 10 seconds...
 timeout 10
 shutdown -s -t 10
